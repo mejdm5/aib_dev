4fb8aceabb23a7775a3564fc1fb94f2b5f68d756
Mejak <dmejak@microage.com>
2020-10-27 09:25:56 -0400

    test

--- powershell/aibtest.ps1
+++ powershell/aibtest.ps1
@@ -33,4 +33,14 @@ New-Item -Path 'HKLM:\SOFTWARE\Citrix\PortICA' -Force | Out-Null
 Invoke-WebRequest -Uri 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&download=true&managedInstaller=true&arch=x64' -OutFile 'c:\temp\Teams.msi'
 Invoke-Expression -Command 'msiexec /i C:\temp\Teams.msi /quiet /l*v C:\temp\teamsinstall.log ALLUSER=1'
 Start-Sleep -Seconds 30
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\Cu
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run32 -Name Teams -PropertyType Binary -Value ([byte[]](0x01,0x00,0x00,0x00,0x1a,0x19,0xc3,0xb9,0x62,0x69,0xd5,0x01)) -Force
+
+#Start sleep
+Start-Sleep -Seconds 10
+
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+
+
+
